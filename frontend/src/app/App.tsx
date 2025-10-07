e41c0bb30e1841ea6a284b87770336d4b38ee436
João Sá <joao.vsaa90@gmail.com>
2025-10-07 19:31:14 -0300

    feat: setup home page

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,13 @@
+
+import Auth from "@/pages/Auth";
+import Dashboard from "@/pages/Dashboard";
+
+
+const App = () => (
+
+  <div className="min-h-screen bg-gray-100">
+    <Auth />
+  </div>
+);
+
+export default App;
