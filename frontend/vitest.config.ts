4fb3531c4b9adcd9218e05550639ab12fcf3d348
João Sá <joao.vsaa90@gmail.com>
2025-10-09 21:27:10 -0300

    test: deposit tests

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+    plugins: [tsconfigPaths()],
     test: {
         globals: true,
-        environment: 'node',
-        include: ['tests/api/**/*.spec.ts'],
+        environment: "node",
+        include: ["tests/**/*.spec.ts"],
     },
 });
